ae133f97fa753725db3086f3c503a1d75d76977f
Undecember <ehdtjskaqhd@gmail.com>
2021-07-29 17:30:30 +0900

    Delete countban

--- cogs/Moderator.py
+++ cogs/Moderator.py
@@ -14,13 +14,3 @@ class Core(DBCog):
     async def ModBan(self, ctx, who: discord.User, reason = None):
         await ctx.guild.ban(who, reason = reason, delete_message_days = 7)
         await ctx.send(embed = discord.Embed(title = 'RIP :zany_face:', description = f'**{who.name}#{who.discriminator}**'))
-
-    @commands.command(name = 'countban')
-    @commands.has_guild_permissions(administrator = True)
-    async def CountBan(self, ctx, count: int):
-        await ctx.message.delete()
-        msgs = await ctx.channel.history(limit = count).flatten()
-        for msg in msgs:
-            who = msg.author
-            if ctx.guild.get_member(who.id): continue
-            await ctx.guild.ban(who, delete_message_days = 7)
