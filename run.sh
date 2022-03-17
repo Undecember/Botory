aa45fc9039df5ea62893672b00602501d3e5681c
Undecember <ehdtjskaqhd@gmail.com>
2022-03-17 17:25:07 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 mkdir -p logs
 chown undec:undec logs
 timecode=$(date +%Y.%m.%d-%H.%M.%S)
-node . > logs/$timecode.log 2> logs/$timecode.error.log
+node . > logs/latest.log 2> logs/latest.error.log
+cd logs
+mv latest.log $timecode.log
+mv latest.error.log $timecode.error.log
+xz $timecode.log
+xz $timecode.error.log
